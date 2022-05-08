68d158fdb5bd428e0d22d35d9ef7db3420db7832
AydanMurshud <aydinmurshud@gmail.com>
2022-05-08 19:15:44 +0300

    * Light/Dark mode implemented * Responsive design updated added some svg

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,8 @@ const sideMenuclosed = document.getElementById('h-menu-closed');
 const mainPage = document.getElementById('main-page');
 const profilePicOpened = document.getElementById('pp-opened');
 const profilePicClosed = document.getElementById('pp-closed');
+const modeSwitch = document.getElementById('mode');
+const mainContainer = document.getElementById('container');
 
 function menuOpen() {
     hMenu.addEventListener('click', () => {
@@ -32,3 +34,36 @@ function menuClose() {
 }
 menuClose()
 
+function switchMode() {
+    let counter = 0;
+    modeSwitch.addEventListener('click', () => {
+        if (counter === 0) {
+            mainContainer.style.backgroundImage = 'url(/assets/images/hexagon-svgrepo-com-bg.svg)'
+            mainContainer.style.backgroundColor = 'black';
+            mainContainer.style.backgroundSize = '250px';
+            mainContainer.style.backgroundPosition = 'bottom';
+            document.documentElement.style.setProperty('--thirdColor', '#FFFFFF')
+            sideMenuclosed.style.backgroundColor = 'rgb(17, 17, 17)';
+            sideMenuclosed.style.backgroundImage = 'none'
+            sideMenuOpen.style.backgroundColor = 'rgb(17, 17, 17)';
+            sideMenuOpen.style.backgroundImage = 'none'
+            
+            modeSwitch.innerHTML = '🌑'
+            counter++;
+        } else {
+            mainContainer.style.backgroundImage = 'url(/assets/images/19366.jpg)'
+            mainContainer.style.backgroundColor = 'none';
+            mainContainer.style.backgroundSize = 'cover'
+            mainContainer.style.backgroundPosition = 'none';
+            document.documentElement.style.setProperty('--thirdColor', '#060b38')
+            sideMenuclosed.style.backgroundColor = 'var(--mainColor)';
+            sideMenuclosed.style.backgroundImage = 'url(/assets/images/hexagon-svgrepo-com.svg)'
+            sideMenuOpen.style.backgroundColor = 'var(--mainColor)';
+            sideMenuOpen.style.backgroundImage = 'url(/assets/images/hexagon-svgrepo-com.svg)'
+
+            modeSwitch.innerHTML = '☀️'
+            counter--;
+       }
+    });
+}
+switchMode()
